8a14526f3c869a76a6dbf7303b602234d5855466
Jack Hansen <jack@Jacks-MacBook-Pro.local>
2018-10-11 23:36:34 -0600

    for some reason config was in git-ignore

---
+++ src/config/strategies/google.strategy.js
@@ -0,0 +1,63 @@
+const passport = require("passport");
+const { Strategy } = require("passport-custom");
+const { OAuth2Client } = require("google-auth-library");
+const { MongoClient } = require("mongodb");
+const debug = require("debug")("app.google.strategy");
+
+const mongoConfig = require("../mongo_config");
+const { clientID } = require("../api_keys");
+
+const authClient = new OAuth2Client(clientID);
+
+async function getUser(username, done) {
+  const url = mongoConfig.db.host;
+  const db = mongoConfig.db.name;
+
+  let client;
+
+  try {
+    client = await MongoClient.connect(url);
+    debug("Connected to mongo server");
+
+    const database = await client.db(db);
+    const col = await database.collection("users");
+    let user = await col.findOne({ username });
+
+    if (user !== null) {
+      done(null, user);
+    } else {
+      col.insertOne({ username });
+      user = await col.findOne({ username });
+      done(null, user);
+    }
+  } catch (err) {
+    debug(err.stack);
+  }
+
+  client.close();
+}
+
+function googleStrategy() {
+  passport.use(
+    "google-strategy",
+    new Strategy(async (req, done) => {
+      const { tokenID, username } = req.params;
+
+      const ticket = await authClient.verifyIdToken({
+        idToken: tokenID,
+        audience: clientID
+      });
+
+      const payload = ticket.getPayload();
+      debug(payload);
+
+      if (payload !== null) {
+        getUser(username, done);
+      } else {
+        done(null, false);
+      }
+    })
+  );
+}
+
+module.exports = googleStrategy;
