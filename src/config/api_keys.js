31c47bd9e54c231df3b1a190e37f62d49104a3a9
Jack Hansen <jack@Jacks-MacBook-Pro.local>
2018-10-13 10:53:04 -0600

    api key bug fix 2

--- src/config/api_keys.js
+++ src/config/api_keys.js
@@ -2,4 +2,4 @@ const apiKey = "6ea89408-e67e-4a4e-8e36-c8b51c86c7df";
 const clientID = "1076348502483-94jj00g9uklm1cu15er14ehs22qvc7g3.apps.googleusercontent.com";
 const secret = "wL0OdOHvkath2a83yBuTvO-B";
 
-module.exports = [apiKey, clientID, secret];
+module.exports = { apiKey, clientID, secret };
