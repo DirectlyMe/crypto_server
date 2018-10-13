27464fb0e05ef04076683fa89b275c65375c811b
Jack Hansen <jack@Jacks-MacBook-Pro.local>
2018-10-13 10:45:10 -0600

    api_key bug fixed

--- src/config/api_keys.js
+++ src/config/api_keys.js
@@ -1,4 +1,4 @@
-const apiKey = { api_key: "6ea89408-e67e-4a4e-8e36-c8b51c86c7df" };
+const apiKey = "6ea89408-e67e-4a4e-8e36-c8b51c86c7df";
 const clientID = "1076348502483-94jj00g9uklm1cu15er14ehs22qvc7g3.apps.googleusercontent.com";
 const secret = "wL0OdOHvkath2a83yBuTvO-B";
 
